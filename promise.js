fd4340e0fe591b582adc6ae932b735f926a8591f
Henni <henni.yulianti@unpad.ac.id>
2022-06-12 19:33:22 +0700

    solve challenge Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (anger) => {
+  return Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+    .then((response) => {
+      const [ixx, vgc] = response;
+      return [...ixx, ...vgc].filter((item) => item.hasil === anger).length;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 module.exports = {
   promiseOutput,
